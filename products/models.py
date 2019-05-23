817c1b2b4465dee81db38180b58a3a1c8ba02b7d
Vikas6206 <vikas.kr6206@gmail.com>
2019-05-23 18:41:13 +0530

    Doing the migration for the model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code= models.CharField(max_length=255)
+    description =models.CharField(max_length=255)
+    discount= models.FloatField()
+
+# Create a model class
+# Go to the settings.py and umder installed apps add the path to the product project
+# Type the following command for making migration
+#  python manage.py makemigrations
+# To manage the migration use the following command
+# python manage.py migrate
+# Use the sqllite tool to import the db.sqlite3 file
+
+
+
